93b722a99e79101f446091bab6f0d10239eb9aed
Agung Hidayat <agunghidayat10@gmail.com>
2021-12-02 14:30:10 +0700

    FEAT: add card and responsive layout grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-gray-300">
+    <div class="container mx-auto py-8 grid grid-cols-1 gap-4 p-5 sm:grid-cols-1 sm:gap-6 md:grid-cols-2 md:gap-8 md:px-0 lg:grid-cols-3 lg:gap-12 lg:px-0 transition-all duration-200 ease-linear">
+      <Card v-for="n in 10" :key="`card-${n}`" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
+import "./tailwind.css";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+      Card,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full bg-white rounded-2xl shadow-md overflow-hidden">
+        <div class="w-full overflow-hidden" id="banner">
+            <img class="object-cover min-h-100 w-full hover:scale-110 transition-transform duration-300 ease-in-out" src="../assets/banner.png" alt="banner">
+        </div>
+        <div id="content" class="px-[25px] py-[16px]">
+            <div id="header">
+                <h5 class="font-bold text-[24px] text-primary">Bahasa Indonesia</h5>
+                <p class="text-gray-400 text-[16px] mt-[6px]">Kelas | Sekolah Dasar</p>
+            </div>
+            <div id="profile" class="flex mt-[32px] self-center">
+                <div class="rounded-full w-[59px] overflow-hidden mr-[15px]">
+                    <img class="object-cover" src="../assets/profile.png" alt="profile">
+                </div>
+            <div>
+                <h5 class="font-medium text-[20px]">Cici Sang Putri</h5>
+                <p class="text-gray-400 text-[16px]">Kelas | Sekolah Dasar</p>
+            </div>
+            </div>
+                <div id="footer" class="flex justify-between items-center mt-[38px]">
+                <span class="text-xl font-bold text-primary">Rp.90.000</span>
+                <button class="py-[8px] px-[16px] rounded-lg ring-[2px] font-medium ring-blue-900 text-blue-900 hover:text-white hover:bg-blue-900 transition-all duration-200 ease-in-out">Lihat Kelas</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+</script>
